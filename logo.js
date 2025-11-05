49a375a17edf63cab8a7c7473b96838e30a99274
timesapp.it <iosifcastrucci@icloud.com>
2025-11-05 15:14:58 +0100

    Update and rename script.js to logo.js

---
+++ logo.js
@@ -0,0 +1,9 @@
+// Wait 5 seconds, then fade out and redirect to index.html
+setTimeout(() => {
+  document.querySelector('.loader-container').style.transition = 'opacity 1.5s ease';
+  document.querySelector('.loader-container').style.opacity = '0';
+
+  setTimeout(() => {
+    window.location.href = "index.html";
+  }, 1500);
+}, 5000);
